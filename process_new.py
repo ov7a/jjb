5add56af8014b1a657817c2d04eb949e1ed075a5
ov7a <ov7a@yandex.ru>
2017-09-17 15:55:22 +0300

    Email notifications about possible copies

--- process_new.py
+++ process_new.py
@@ -6,32 +6,59 @@ import argparse
 import yaml
 from ejudge_client import EjudgeClient
 import handlers
+from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from subprocess import Popen, PIPE
 
 def format_floats(floats):
 	return ", ".join(map(lambda x: "%0.2f" % x if x else "None", floats))
 
-def alert(old_run, new_run, alerts):
+def create_attachment(prefix, run, run_source):
+	name = "%s_%d_%s" % (prefix, run.id, run.user_login)
+	attachment = MIMEApplication(run_source, Name=name)
+	attachment['Content-Disposition'] = 'attachment; filename="%s"' % name
+	return attachment
+
+def sendmail(old_run, old_run_source, new_run, new_run_source, alerts, mail):
+	text = "%s\nis probably a copy of\n%s\nSimilarities:\n" % (str(new_run), str(old_run))
+	text += "\n".join(map(lambda x: "%s\t%0.2f" % x if x[1] is not None else "%s\t%s" % x, alerts))
+	
+	message = MIMEMultipart()
+	message["To"] = mail
+	message["Subject"] = "Suspicious run: %d, %s, %s, %s" % (run.id, run.problem, run.lang, run.user_login)
+	
+	message.attach(MIMEText(text))
+	message.attach(create_attachment("original", old_run, old_run_source))
+	message.attach(create_attachment("copy", new_run, new_run_source))
+	
+	p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+	p.communicate(message.as_bytes())
+
+def alert(old_run, old_run_source, new_run, new_run_source, alerts, alert_mail):
 	(names, similarities) = zip(*alerts)
 	logging.info("%s is a copy of %s, according to %s. Similarities: (%s)", str(new_run), str(old_run), ", ".join(names), format_floats(similarities))
+	if alert_mail is not None:
+		sendmail(old_run, old_run_source, new_run, new_run_source, alerts, alert_mail)
 
 def get_similar_runs_filter(run):
 	return '(status == OK or status == DQ) and login !="%s" and prob == "%s"' % (run.user_login, run.problem)
 
-def compare_runs(old_run, old_run_source, new_run, new_run_source, checkers):
+def compare_runs(old_run, old_run_source, new_run, new_run_source, checkers, alert_mail):
 	logging.info("Comparing runs %s and %s", old_run, new_run)
 	all_results = []
 	alerts = []
 	for checker in checkers:
 		handler = handlers.get(checker['type'])
 		similarity = handler(checker, old_run_source, new_run_source, new_run.lang)
-		if ((not (similarity is None)) and (similarity > checker['threshold'])) or ((similarity is None) and checker.get('alert_if_none', True)):
+		if ((similarity is not None) and (similarity > checker['threshold'])) or ((similarity is None) and checker.get('alert_if_none', True)):
 			alerts.append((checker['name'], similarity))
 		all_results.append(similarity)
 	if len(alerts):
-		alert(old_run, new_run, alerts)
+		alert(old_run, old_run_source, new_run, new_run_source, alerts, alert_mail)
 	logging.info("Similarity between %d and %d is (%s)", old_run.id, new_run.id, format_floats(all_results))
 
-def check_run(client, run, checkers):
+def check_run(client, run, checkers, alert_mail):
 	runs_filter = get_similar_runs_filter(run)
 	previous_runs = client.get_runs(0, run.id - 1, runs_filter)
 	if not len(previous_runs):
@@ -40,7 +67,7 @@ def check_run(client, run, checkers):
 	run_source = client.get_run_source(run.id)
 	for old_run in previous_runs:
 		old_run_source = client.get_run_source(old_run.id)
-		compare_runs(old_run, old_run_source, run, run_source, checkers)
+		compare_runs(old_run, old_run_source, run, run_source, checkers, alert_mail)
 
 
 def get_args():
@@ -73,6 +100,7 @@ if __name__=="__main__":
 	
 	checkers = config['checkers']
 	last_run_file = config['last_run_file'] % args.contest_id
+	alert_mail = config.get('alert_mail')
 
 	try:
 		with open(last_run_file) as f:
@@ -91,7 +119,7 @@ if __name__=="__main__":
 	runs = client.get_runs(last_run + 1)
 	for run in runs:
 		logging.info("Checking run %s", str(run))
-		check_run(client, run, checkers)
+		check_run(client, run, checkers, alert_mail)
 
 	if len(runs):
 		max_run = max(runs, key=lambda x: x.id).id
