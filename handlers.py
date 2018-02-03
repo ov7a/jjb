14c3640d3ba82b59002dd98c43f0339fce8039c9
ov7a <ov7a@yandex.ru>
2018-02-03 13:08:23 +0300

    retry with temp files for external tool

--- handlers.py
+++ handlers.py
@@ -19,36 +19,42 @@ def goldfish(config, old_run, new_run):
 	result = jjb.similarity(old_tokens, new_tokens)
 	return result	
 
-def external(config, old_run, new_run):
+def __try_external(config, old_run, new_run, force_temp=False):
 	args = config['args']
 	
-	(old_is_temp, old_path) = get_source_path(old_run)
-	(new_is_temp, new_path) = get_source_path(new_run)
+	(old_is_temp, old_path) = get_source_path(old_run, force_temp)
+	(new_is_temp, new_path) = get_source_path(new_run, force_temp)
 		
 	all_args = list(map(str, args)) + [new_run.lang, old_path, new_path]
 	try:
 		output = subprocess.check_output(all_args).decode('utf-8')
 		result = float(output)
 	except subprocess.CalledProcessError:	
-		if (old_run.lang != new_run.lang):
-			result = 0.0
-		else:	
-			result = None					
+		result = None					
 	if old_is_temp:
 		os.remove(old_path)
 	if new_is_temp:
 		os.remove(new_path)
 	return result
 
-all_handlers = dict(inspect.getmembers(sys.modules[__name__], predicate=lambda x: isinstance(x, types.FunctionType)))
+def external(config, old_run, new_run):
+	result = __try_external(config, old_run, new_run)
+	if result is None:
+		result = __try_external(config, old_run, new_run, force_temp=True)
+	if (result is None) and (old_run.lang != new_run.lang):
+		result = 0.0
+	return result	
+
+all_handlers = dict(inspect.getmembers(sys.modules[__name__], predicate=lambda x: isinstance(x, types.FunctionType) and (not x.__name__.startswith("__"))))
 
 def get(handler_name):
 	return all_handlers[handler_name]		
 	
-def get_source_path(run):
-	path = run.source_path()
-	if (os.path.isfile(path)):
-		return (False, path)
+def get_source_path(run, force_temp):
+	if (not force_temp):
+		path = run.source_path()
+		if (os.path.isfile(path)):
+			return (False, path)
 		
 	temp_file = tempfile.NamedTemporaryFile(delete=False)
 	with temp_file as temp:
